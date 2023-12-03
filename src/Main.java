7a4669f4330a2a846643113dda7a84285d5ed084
rolling01pepper <rolling_pepper@vk.com>
2023-12-03 21:48:44 +0100

    Add files

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[] {0,1,234,4,5,6};
+        int target = 234;
+        System.out.println(bs(arr, target));
+    }
+    public static int bs(int[] arr, int target){
+        int l = 0;
+        int r = arr.length - 1;
+        while (l<=r){
+            int m = l + (r-l)/2;
+            if (arr[m] > target){
+                r = m - 1;
+            } else if (arr[m] < target){
+                l = m + 1;
+            } else return m;
+        }
+        return -1;
+    }
+}
